8055e4f06f2b4002b93dc25b88a6721f253119b1
SharmanJeurkar <kirtisjeurkar@gmail.com>
2023-04-14 16:55:02 +0530

    added couple of features, for the home page

---
+++ CommandLineBankSystem/src/com/bank/features/Home.java
@@ -0,0 +1,16 @@
+package com.bank.features;
+
+public class Home {
+	
+	public void printFeatures() {
+		
+		String features[]= {"Credit", "Debit"};
+		
+		for(int i =0; i<features.length;i++) {
+			System.out.println(features[i]);
+		}
+
+		
+	}
+	
+}
