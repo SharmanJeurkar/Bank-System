24ddcde5345386565fbff87f13d7c99163830ed6
SharmanJeurkar <kirtisjeurkar@gmail.com>
2023-04-14 19:17:38 +0530

    closing issue #4

--- CommandLineBankSystem/src/com/bank/Main.java
+++ CommandLineBankSystem/src/com/bank/Main.java
@@ -14,7 +14,17 @@ public class Main {
 		if( loginSuccesfull== true) {
 			home.printFeatures();
 		}else {
-			obj1.login();
+			for(int i=0; i<=5; i++) {
+				if(i<5) {
+					System.out.println("Retry logging in..... Attempt "+i);
+					obj1.login();
+				}else {
+					System.err.println("Login credentials aren't correct!");
+					System.exit(1);
+					return;
+				}
+			}
+			
 		}
 		
 	}
