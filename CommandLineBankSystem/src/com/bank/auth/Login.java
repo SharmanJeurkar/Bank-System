e2c995e768fae8ea754367df55b9a09db75e005e
SharmanJeurkar <kirtisjeurkar@gmail.com>
2023-04-14 16:09:19 +0530

    added login for auth

---
+++ CommandLineBankSystem/src/com/bank/auth/Login.java
@@ -0,0 +1,29 @@
+package com.bank.auth;
+
+import java.util.Scanner;
+
+public class Login {
+	
+	public boolean login() {
+		
+		Scanner obj = new Scanner(System.in);
+		String uname = "root";
+		String upass = "root";
+		
+		System.out.println("Username: ");
+		String iuname = obj.next();
+		
+		System.out.println("Password: ");
+		String ipass = obj.next();
+		
+		if(iuname.equals(uname) && upass.equals(upass)) {
+			System.out.println("Login Succesfull......");
+			return true;
+		}else {
+			System.out.println("Logn unsucessfull...........");
+			return false;
+		}
+		
+	}
+
+}
