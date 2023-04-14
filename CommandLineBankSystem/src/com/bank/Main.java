c901e8c1f65d84b7ee914cefbcb4b8a0aa442c52
SharmanJeurkar <kirtisjeurkar@gmail.com>
2023-04-14 16:58:26 +0530

    added the printFeatures function from commit 8055e4f

--- CommandLineBankSystem/src/com/bank/Main.java
+++ CommandLineBankSystem/src/com/bank/Main.java
@@ -1,13 +1,19 @@
 package com.bank;
 
 import com.bank.auth.Login;
+import com.bank.features.Home;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		Login obj1 = new Login();
+		Home home = new Home();
 		
 		obj1.login();
+		
+		if(obj1.login() == true) {
+			home.printFeatures();
+		}
 		
 	}
 	
