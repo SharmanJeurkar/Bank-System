b417f67ba4d511f36d5ba82d7e6263c26435455a
SharmanJeurkar <kirtisjeurkar@gmail.com>
2023-04-15 16:40:47 +0530

    created dynamacy in array of options of features

--- CommandLineBankSystem/src/com/bank/features/Home.java
+++ CommandLineBankSystem/src/com/bank/features/Home.java
@@ -1,16 +1,32 @@
 package com.bank.features;
 
+import java.util.Scanner;
+
 public class Home {
 	
+	Scanner scanner = new Scanner(System.in);
+	String features[]= {"Credit", "Debit","Delete account"};
+	
 	public void printFeatures() {
-		
-		String features[]= {"Credit", "Debit"};
 		
 		for(int i =0; i<features.length;i++) {
 			System.out.println(features[i]);
 		}
-
 		
+	}
+	
+	public void selectFeature() {
+		
+		System.out.println("Select one of the above options");
+		String selectedOption = scanner.nextLine();
+		
+		for (String string : features) {
+		
+		if(selectedOption.equalsIgnoreCase(string = "Credit")) {
+			System.out.println("Hello credit");
+		}
+		
+		}
 	}
 	
 }
