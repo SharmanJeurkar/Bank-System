03be23c98f8765942f4a3e212661f828fb870f9e
SharmanJeurkar <kirtisjeurkar@gmail.com>
2023-04-15 16:46:12 +0530

    added a function for selection of features

--- CommandLineBankSystem/src/com/bank/features/Home.java
+++ CommandLineBankSystem/src/com/bank/features/Home.java
@@ -5,27 +5,29 @@ import java.util.Scanner;
 public class Home {
 	
 	Scanner scanner = new Scanner(System.in);
-	String features[]= {"Credit", "Debit","Delete account"};
+	String creditoption = "Credit";
+	String debitOption = "Debit";
 	
 	public void printFeatures() {
-		
+		String features[]= {creditoption, debitOption};
 		for(int i =0; i<features.length;i++) {
 			System.out.println(features[i]);
 		}
 		
 	}
 	
-	public void selectFeature() {
+public void selectFeature() {
 		
 		System.out.println("Select one of the above options");
 		String selectedOption = scanner.nextLine();
 		
-		for (String string : features) {
 		
-		if(selectedOption.equalsIgnoreCase(string = "Credit")) {
+		
+		if(selectedOption.equalsIgnoreCase(creditoption)) {
 			System.out.println("Hello credit");
 		}
-		
+		else if(selectedOption.equalsIgnoreCase(debitOption)) {
+			System.out.println("Hello debit");
 		}
 	}
 	
