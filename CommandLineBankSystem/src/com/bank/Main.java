84ab091b2e51f4dbf85796652d60a2aa4f001471
SharmanJeurkar <kirtisjeurkar@gmail.com>
2023-04-18 19:06:43 +0530

    added exception for implementation of hom.features() function

--- CommandLineBankSystem/src/com/bank/Main.java
+++ CommandLineBankSystem/src/com/bank/Main.java
@@ -5,7 +5,7 @@ import com.bank.features.Home;
 
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Login obj1 = new Login();
 		Home home = new Home();
 		
