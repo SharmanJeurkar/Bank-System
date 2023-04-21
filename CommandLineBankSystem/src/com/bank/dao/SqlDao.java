efebd2d6e63c2bfa12aedf11a168bca9e189d5ba
SharmanJeurkar <kirtisjeurkar@gmail.com>
2023-04-21 15:33:00 +0530

    added all the sql duplicate methods from credit_dao and debit_dao are added

--- CommandLineBankSystem/src/com/bank/dao/SqlDao.java
+++ CommandLineBankSystem/src/com/bank/dao/SqlDao.java
@@ -1,5 +1,37 @@
 package com.bank.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class SqlDao {
+	
+	Connector connector =  new Connector();
+	Connection connection;
+	Statement st;
+	ResultSet rs;
+	
+	public long fetchAmountInAccount(long accountNumber) throws Exception {
+			
+			String query = "select * from user_account where Account_Number = " + accountNumber;
+			connection = connector.Connect();
+			
+			st = connection.createStatement();
+			rs=st.executeQuery(query);
+			
+			rs.next();
+			Long ammountInAccount = rs.getLong(3);
+			
+			return ammountInAccount;
+		}
+
+	public  void insertValuesInDb(long accountNumber,long sum) throws Exception {
+		String query =  "update user_account set  Amount_IN_Account="+sum+" where Account_Number= "+accountNumber+";";
+		st=connection.createStatement();
+		st.executeUpdate(query);
+		
+		System.out.println("Account updated....");
+	}
+
 
 }
